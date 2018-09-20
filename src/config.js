55176ff73b0809dad997934a7b9d2a3f205ebfd4
Daniel <Shamaran85@gmail.com>
2018-09-20 18:51:33 +0200

    Updated config urls

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ module.exports = {
 
     imageUrl: 'https://aqueous-spire-38645.herokuapp.com/',
     productsUrl: 'https://aqueous-spire-38645.herokuapp.com/products/',
-    ratingsUrl: 'http://localhost:1337/ratings/?productId=',
-    orderUrl: 'http://localhost:1337/orders/',
-    fillterHoodieUrl: 'http://localhost:1337/products/?category=Hoodies',
+    ratingsUrl: 'https://aqueous-spire-38645.herokuapp.com/ratings/?productId=',
+    orderUrl: 'https://aqueous-spire-38645.herokuapp.com/orders/',
+    fillterHoodieUrl: 'https://aqueous-spire-38645.herokuapp.com/products/?category=Hoodies',
 
 }
